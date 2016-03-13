e01ce17194a05cf13f43b1c045cbec0689bd62dd
Hafidh Fikri Rasyid <hafidhfikri.rasyid@gmail.com>
2016-03-13 09:21:12 +0700

    Tugas Besar PBO 2016 Phase 1

--- src/tugasbesarpbo2016/Pelanggan.java
+++ src/tugasbesarpbo2016/Pelanggan.java
@@ -10,14 +10,14 @@ package tugasbesarpbo2016;
  * @author Hafidh Fikri Rasyid
  */
 public class Pelanggan extends Orang {
-    private Pesanan pesanan[];
+    private Pesanan pesanan[] = new Pesanan[100];;
     private long id_pelanggan;
     private String alamat_pelanggan;
     private int jml_pesanan;
 
     public Pelanggan(String nama, long no_telp, String alamat_pelanggan, long id_pelanggan){
-        this.nama = nama;
-        this.no_telp = no_telp;
+        super.setNama(nama);
+        super.setNo_telp(no_telp);
         this.alamat_pelanggan = alamat_pelanggan;
         this.id_pelanggan = id_pelanggan;
     }
@@ -38,12 +38,58 @@ public class Pelanggan extends Orang {
         this.alamat_pelanggan = alamat_pelanggan;
     }
     
-    public void createPesanan(){
-        pesanan[jml_pesanan] = new Pesanan();
-        jml_pesanan++;
+    public void createPesanan(Pesanan p){
+        if (jml_pesanan<100){
+            pesanan[jml_pesanan] = p;
+            jml_pesanan++;
+        }else{
+            System.out.println("Array Pesanan Penuh !!!");
+        }
+    }
+    
+    public void createPesananKurir(Kurir k){
+        if (jml_pesanan<100){
+            pesanan[jml_pesanan] = k;
+            jml_pesanan++;
+        }else{
+            System.out.println("Array Pesanan Penuh !!!");
+        }
     }
     
     public Pesanan getPesanan(int index){
         return pesanan[index];
+    }
+    
+    public boolean removePesanan(long id_pesanan){
+        for (int j = 0; j < jml_pesanan; j++) {
+            if(pesanan[j].getId_pesanan() == id_pesanan){
+                //cek dia index keberapa
+                if(j == 0){
+                    //index pertama alias delete first
+                    pesanan[0] = null;
+                    for (int k = 0; k < jml_pesanan-1; k++) {
+                        pesanan[k] = pesanan[k+1];
+                    }
+                    pesanan[jml_pesanan] = null;
+                    jml_pesanan--;
+                    return true;
+                }else if(j == jml_pesanan){
+                    //index terakhir alias delete last
+                    pesanan[j] = null;
+                    jml_pesanan--;
+                    return true;
+                }else{
+                    //index tengah alias delete after
+                    pesanan[j] = null;
+                    for (int k = j; k < jml_pesanan-1; k++) {
+                        pesanan[k] = pesanan[k+1];
+                    }
+                    pesanan[jml_pesanan] = null;
+                    jml_pesanan--;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/tugasbesarpbo2016/Pesanan.java
+++ src/tugasbesarpbo2016/Pesanan.java
@@ -10,5 +10,70 @@ package tugasbesarpbo2016;
  * @author Hafidh Fikri Rasyid
  */
 public class Pesanan {
+    private long id_pesanan;
+    private int tarif;
+    private String alamat_tujuan;
+    private String Status;
+    private int jarak;
+    private int jenis;
+    private int berat;
     
+    public Pesanan(String alamat_tujuan, String Status, long id_pesanan){
+        this.alamat_tujuan = alamat_tujuan;
+        this.Status = Status;
+        this.id_pesanan = id_pesanan;
+    }
+        
+    public void createTarif(int jenis) {
+        this.jenis = jenis;
+    }
+    
+    public void createTariff(int jenis, int berat) {
+        this.jenis = jenis;
+        this.berat = berat;
+    }
+    
+    public void ubahStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public long getId_pesanan() {
+        return id_pesanan;
+    }
+
+    public void setId_pesanan(long id_pesanan) {
+        this.id_pesanan = id_pesanan;
+    }
+
+    public int getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(int tarif) {
+        this.tarif = tarif;
+    }
+
+    public String getAlamat_tujuan() {
+        return alamat_tujuan;
+    }
+
+    public void setAlamat_tujuan(String alamat_tujuan) {
+        this.alamat_tujuan = alamat_tujuan;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public int getJarak() {
+        return jarak;
+    }
+
+    public void setJarak(int jarak) {
+        this.jarak = jarak;
+    }
 }
